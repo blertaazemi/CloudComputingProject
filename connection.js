8cadc7ef72dd195f8bfccf3ef6e47c295ca346e4
BlerttaKrasniqi <122098017+BlerttaKrasniqi@users.noreply.github.com>
2024-05-31 23:40:49 +0200

    main: fixed connection.js

--- connection.js
+++ connection.js
@@ -11,23 +11,16 @@ const config = {
   },
 };
 
-async function getConnection() {
-  try {
-    await sql.connect(config);
-    console.log('Connected to Azure SQL Database');
-    await readData();
-  } catch (err) {
-    console.error('Failed to connect to Azure SQL Database', err);
-  }
-}
+let pool;
 
-async function readData() {
-  try {
-    const result = await sql.query('SELECT * FROM Library_Books');
-    console.log(result.recordset);
-  } catch (err) {
-    console.error('Error querying database:', err);
+async function getConnection() {
+  if (!pool) {
+    pool = await sql.connect(config);
+    console.log('Connected to Azure SQL Database');
   }
+  return pool;
 }
 
 getConnection();
+
+module.exports = { getConnection, sql };
